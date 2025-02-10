925552af35239b5df0b9cdbd836a787706bd2001
Steve Onyeneke <bobsarglobal@gmail.com>
2025-02-10 16:43:30 +0000

    create website layout

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Wazobia"
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "Shop smart not hard."
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || APP_SLOGAN
