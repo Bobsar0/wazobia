961d6b428d11c6d9a274f6584f29be320a882db7
Steve Onyeneke <bobsarglobal@gmail.com>
2025-02-12 11:33:20 +0000

    create home cards

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,71 @@
-import { HomeCarousel } from "@/components/home/home-carousel";
-import data from "@/lib/data";
+import { HomeCarousel } from '@/components/home/home-carousel'
+import { HomeCards } from '@/components/shared/home/home-cards'
+import {
+  getAllCategories,
+  getProductsForCard,
+} from '@/lib/actions/product.actions'
+import data from '@/lib/data'
+import { toSlug } from '@/lib/utils'
 
 export default async function Home() {
-  return <HomeCarousel items={data.carousels} />;
+  const categories = (await getAllCategories()).slice(0, 4)
+  const newArrivals = await getProductsForCard({
+    tag: 'new-arrival',
+    limit: 4,
+  })
+  const featuredProducts = await getProductsForCard({
+    tag: 'featured',
+    limit: 4,
+  })
+  const bestSellers = await getProductsForCard({
+    tag: 'best-seller',
+    limit: 4,
+  })
+
+  const cards = [
+    {
+      title: 'Categories to explore',
+      link: {
+        text: 'See More',
+        href: '/search',
+      },
+      items: categories.map((category) => ({
+        name: category,
+        image: `/images/${toSlug(category)}.jpg`,
+        href: `/search?category=${category}`,
+      })),
+    },
+    {
+      title: 'Explore New Arrivals',
+      link: {
+        text: 'View All',
+        href: '/search?tag=new-arrival',
+      },
+      items: newArrivals,
+    },
+    {
+      title: 'Discover Best Sellers',
+      link: {
+        text: 'View All',
+        href: '/search?tag=best-seller',
+      },
+      items: bestSellers,
+    },
+    {
+      title: 'Featured Products',
+      link: {
+        text: 'Shop Now',
+        href: '/search?tag=featured',
+      },
+      items: featuredProducts,
+    },
+  ]
+  return (
+    <>
+      <HomeCarousel items={data.carousels} />
+      <div className='md:p-4 md:space-y-4 bg-border'>
+        <HomeCards cards={cards} />
+      </div>
+    </>
+  )
 }
---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,48 @@
+'use server'
+
+import { connectToDatabase } from '@/lib/db'
+import Product from '../db/model/product.model'
+
+
+/**
+ * Returns an array of strings representing all the categories of products.
+ */
+export async function getAllCategories() {
+  await connectToDatabase()
+  const categories = await Product.find({ isPublished: true }).distinct(
+    'category'
+  )
+  return categories
+}
+
+/**
+ * Retrieves a list of products for a given tag, limited by the number provided.
+ * @param {Object} param
+ * @param {string} param.tag - The tag to search products by.
+ * @param {number} [param.limit=4] - The maximum number of products to return.
+ * @returns {Promise<IProductForCard[]>}
+ */
+export async function getProductsForCard({
+  tag,
+  limit = 4,
+}: {
+  tag: string
+  limit?: number
+}) {
+  await connectToDatabase()
+  const products = await Product.find(
+    { tags: { $in: [tag] }, isPublished: true },
+    {
+      name: 1, //get image name
+      href: { $concat: ['/product/', '$slug'] }, //concatenate product path and slug
+      image: { $arrayElemAt: ['$images', 0] }, //get first item of image array
+    }
+  )
+    .sort({ createdAt: 'desc' })
+    .limit(limit)
+  return JSON.parse(JSON.stringify(products)) as {
+    name: string
+    href: string
+    image: string
+  }[]
+}
